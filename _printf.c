fa350af29258afa3c7abcb1a42a7de6d0bed1e0f
chihab37 <chihab37@gmail.com>
2023-04-23 04:35:37 -0700

    fix errors

--- _printf.c
+++ _printf.c
@@ -1,9 +1,66 @@
 #include "main.h"
 /**
+  *my_print_char - prints character passed in as arg
+  *@args: va_list arg
+  *@count: current count of arguments printed
+  *Return: count
+  */
+int my_print_char(va_list args, int count)
+{
+	count += _putchar(va_arg(args, int));
+	return (count);
+}
+
+/**
+  *my_print_string - print a string passed in as arg
+  *@args: va_list arg
+  *@count: ...
+  *Return: count
+  */
+
+int my_print_string(va_list args, int count)
+{
+	char *str = va_arg(args, char *);
+		while (*str != '\0')
+		{
+			count += _putchar(*str);
+			str++;
+		}
+	return (count);
+}
+/**
+  *my_print_percent - print percent sign
+  *@count: ...
+  *Return: count
+  */
+
+int my_print_percent(int count)
+{
+	count += _putchar('%');
+	return (count);
+}
+
+/**
+  *my_print_default - print a default character if an invalid format spec
+  *is provided
+  *@count: ...
+  *Return: count
+  */
+
+int my_print_default(char format, int count)
+{
+	_putchar('%');
+	_putchar(format);
+	count += 2;
+	return (count);
+}
+
+/**
   *_printf - produce output according to a format
   *@format: character string containing zero or more directives
   *Return: number of characters printed
   */
+
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -18,26 +75,16 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-					count += _putchar(va_arg(args, int));
+					count += my_print_char(args, count);
 					break;
 				case 's':
-					{
-					char *str = va_arg(args, char *);
-
-					while (*str != '\0')
-					{
-						count += _putchar(*str);
-						str++;
-					}
-					}
+					count = my_print_string(args, count);
 					break;
 				case '%':
-					count += _putchar('%');
+					count += my_print_percent(count);
 					break;
 				default:
-					_putchar('%');
-					_putchar(*format);
-					count += 2;
+					count = my_print_default(*format, count);
 					break;
 			}
 		}
--- main.h
+++ main.h
@@ -1,9 +1,12 @@
-#ifndef _PRINTF_H
-#define _PRINTF_H
+#ifndef _PRINTF_H_
+#define _PRINTF_H_
 
 #include <stdarg.h>
 #include <stdio.h>
-
+int my_print_char(va_list args, int count);
+int my_print_string(va_list args, int count);
+int my_print_percent(int count);
+int my_print_default(char format, int count);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
