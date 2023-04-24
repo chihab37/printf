b1ea276854f89a43e28a519350d2b4a15d69c085
chihab37 <chihab37@gmail.com>
2023-04-24 13:08:55 -0700

    trial

--- printf_binary.c
+++ printf_binary.c
@@ -6,37 +6,25 @@
   */
 int printf_binary(va_list arg_list)
 {
-	unsigned int num = 0;
-	int bit_count = 0;
-	int int_arg = 0;
+	unsigned int num = va_arg(arg_list, unsigned int);
+	unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
+	int count = 0;
+	int started = 0;
 
-	int_arg = va_arg(arg_list, int);
-	num = int_arg;
-	if (int_arg < 0)
+	while (mask)
 	{
-		_write('1');
-		int_arg *= -1;
-		num = int_arg;
-		bit_count++;
+		if (num & mask)
+		{
+			_write('1');
+			count++;
+			started = 1;
+		}
+		else if (started || mask == 1)
+		{
+			_write('0');
+			count++;
+		}
+		mask >>= 1;
 	}
-	while (num > 0)
-	{
-		num /= 2;
-		bit_count++;
-	}
-	printf_binary_rec(int_arg);
-	return (bit_count);
-}
-/**
-  *printf_binary_rec - recursively prints an int in binary
-  *@int_arg: int to print
-  */
-void printf_binary_rec(int int_arg)
-{
-	unsigned int num = int_arg;
-
-	if (num / 2)
-		printf_binary_rec(num / 2);
-
-	_write(num % 2 + '0');
+	return (count);
 }
