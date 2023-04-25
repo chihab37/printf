44ab8a2c25cc58b4131dd129288f207b6355c722
aliibrn <alibrne53@gmail.com>
2023-04-25 14:29:55 -0700

    fix

--- printf_uoxX_spec.c
+++ printf_uoxX_spec.c
@@ -1,65 +1,72 @@
-#include "main.h"
+#include "holberton.h"
 /**
-  *_printf_unsigned - print an unsigned decimal integer
-  *@arg_list: vardi list
-  *Return: number of chars printed
-  */
-int _printf_unsigned(va_list arg_list)
+ * print_char - Prints character
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
+ */
+int print_char(va_list list)
 {
-	unsigned int num = va_arg(arg_list, unsigned int);
-	int count = 0;
-	char buffer[20];
-	int i = 0;
-
-	if (num == 0)
-	{
-		_write('0');
-		return (1);
-	}
-
-	while (num > 0)
-	{
-		buffer[i++] = (num % 10) + '0';
-		num /= 10;
-	}
-
-	while (--i >= 0)
-	{
-		_write(buffer[i]);
-		count++;
-	}
-
-	return (count);
+	_write_char(va_arg(list, int));
+	return (1);
 }
+
 /**
-  *_printf_octal - print an unsigned octal integer
-  *@arg_list: va list
-  *Return: number of chars printed
-  */
-int _printf_octal(va_list arg_list)
+ * print_string - Prints a string
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
+ */
+int print_string(va_list list)
 {
-	unsigned int num = va_arg(arg_list, unsigned int);
-	int count = 0;
-	char buffer[20];
-	int i = 0;
+	int i;
+	char *str;
+
+	str = va_arg(list, char *);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+		_write_char(str[i]);
+	return (i);
+}
+
+/**
+ * print_percent - Prints a percent symbol
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
+ */
+int print_percent(__attribute__((unused))va_list list)
+{
+	_write_char('%');
+	return (1);
+}
+
+/**
+ * print_integer - Prints an integer
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
+ */
+int print_integer(va_list list)
+{
+	int num_length;
+
+	num_length = print_number(list);
+	return (num_length);
+}
+
+/**
+ * unsigned_integer - Prints Unsigned integers
+ * @list: List of all of the argumets
+ * Return: a count of the numbers
+ */
+int unsigned_integer(va_list list)
+{
+	unsigned int num;
+
+	num = va_arg(list, unsigned int);
 
 	if (num == 0)
-	{
-		_write('0');
-		return (1);
-	}
+		return (print_unsgined_number(num));
 
-	while (num > 0)
-	{
-		buffer[i++] = (num % 8) + '0';
-		num /= 8;
-	}
-
-	while (--i >= 0)
-
-	{
-		_write(buffer[i]);
-		count++;
-	}
-	return (count);
+	if (num < 1)
+		return (-1);
+	return (print_unsgined_number(num));
 }
